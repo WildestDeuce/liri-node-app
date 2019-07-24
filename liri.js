218a3bc4ddad2d1c97a03eafd99b9aa91db4a2ee
WildestDeuce <48850844+WildestDeuce@users.noreply.github.com>
2019-07-24 09:51:58 -0400

    files are set up id and secret added to .env

--- liri.js
+++ liri.js
@@ -1,2 +1,7 @@
 require("dotenv").config();
+var fs = require("fs");
+var axios = require('axios');
+var moment = require("moment");
+var Spotify = require ("node-spotify-api");
 var keys = require("./keys.js");
+var spotify = new Spotify(keys.spotify);
