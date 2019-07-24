af4af49ce10ab4cb395a418da9fb76ffe6bd7139
WildestDeuce <48850844+WildestDeuce@users.noreply.github.com>
2019-07-24 19:59:02 -0400

    concert and song functions completed

--- liri.js
+++ liri.js
@@ -2,6 +2,76 @@ require("dotenv").config();
 var fs = require("fs");
 var axios = require('axios');
 var moment = require("moment");
-var Spotify = require ("node-spotify-api");
+var Spotify = require("node-spotify-api");
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
+
+var command = process.argv[2];
+var input = process.argv[3];
+
+switch (command) {
+  case "concert-this":
+    concert();
+    break;
+
+  case "spotify-this-song":
+    song();
+    break;
+
+  case "movie-this":
+    // movie();
+    break;
+
+  case "do-what-it-says":
+    //verbatim();
+    break;
+}
+
+function concert() {
+  axios
+    .get("https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp")
+    .then(function (response) {
+      // If the axios was successful...
+      // Then log the body from the site!
+      console.log(response.data[0].venue.name);
+    })
+    .catch(function (error) {
+      if (error.response) {
+      }
+    })
+}
+
+function song() {
+ 
+  spotify.search({ type: 'track', query: input }, function (err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+
+    console.log(data);
+  });
+}
+
+  function verbatim() {
+
+    // We will read the existing bank file
+    fs.readFile("bank.txt", "utf8", function (err, data) {
+      if (err) {
+        return console.log(err);
+      }
+
+      // Break down all the numbers inside
+      data = data.split(", ");
+      var result = 0;
+
+      // Loop through those numbers and add them together to get a sum.
+      for (var i = 0; i < data.length; i++) {
+        if (parseFloat(data[i])) {
+          result += parseFloat(data[i]);
+        }
+      }
+
+      // We will then print the final balance rounded to two decimal places.
+      console.log("You have a total of " + result.toFixed(2));
+    });
+  }
